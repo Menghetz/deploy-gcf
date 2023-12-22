a16a37b4ec554aeae92403c3eed03b56e8eb8882
Menghetz <matteo.menghetti@gmail.com>
2023-12-22 14:39:14 +0100

    First commit

---
+++ test_main.py
@@ -0,0 +1,25 @@
+import main
+import unittest
+import json
+from unittest.mock import Mock
+
+
+class TestCalculations(unittest.TestCase):
+    def test_print_hello_world(self):
+        data = {}
+        req = Mock(get_json=Mock(return_value=data), args=data)
+
+        # Call tested function
+        assert main.hello_world(req) == "Hello World!"
+
+    def test_print_name(self):
+        name = "Bob"
+        data = {"name": name}
+        req = Mock(get_json=Mock(return_value=data), args=data)
+
+        # Call tested function
+        assert main.hello_world(req) == f"Hello {name}!"
+
+
+if __name__ == '__main__':
+    unittest.main()
